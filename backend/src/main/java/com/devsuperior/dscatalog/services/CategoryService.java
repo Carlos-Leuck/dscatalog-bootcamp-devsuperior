570be6fb07941b7e25c094417eb1e14a236931f1
Carlos Leuck <carlinhosrlz@gmail.com>
2021-05-04 10:11:49 -0300

    Database Seeding

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,8 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
+    // only to show how to do a transaction. readOnly = true; Use to avoid "lock" the db. Increases the perfomance.
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
         return repository.findAll();
 
