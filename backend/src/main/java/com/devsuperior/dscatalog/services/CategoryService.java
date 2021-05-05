8c0645565ceef660802b132ce05e4586d6b59616
Carlos Leuck <carlinhosrlz@gmail.com>
2021-05-05 17:44:09 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog.services;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -16,8 +18,10 @@ public class CategoryService {
 
     // only to show how to do a transaction. readOnly = true; Use to avoid "lock" the db. Increases the perfomance.
     @Transactional(readOnly = true)
-    public List<Category> findAll() {
-        return repository.findAll();
+    public List<CategoryDTO> findAll() {
+        List<Category> list = repository.findAll(); // remember! repository works with entity Category.
+        //so we need to use this lambda to convert Category to CategoryDTO.
+        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 
     }
 }
